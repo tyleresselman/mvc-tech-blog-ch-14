b6ce7db9eeab768176fdad7ec135249126779a72
Tyler Esselman <tme91302@gmail.com>
2022-10-11 19:45:15 -0700

    set up heroku and set up keys

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SUPER_SECRET_PASSWORD,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
