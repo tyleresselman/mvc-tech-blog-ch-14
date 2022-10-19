357f81d7a0bc408332838293c9dcd438f0cbf85f
Tyler Esselman <tme91302@gmail.com>
2022-10-18 17:45:41 -0700

    built home routes and dashboard routes

---
+++ controllers/routesDashboard.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { TimeoutError } = require('sequelize');
+const { Post } = require('../models/');
+const auth = require('../utils/auth');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('all-posts', {
+            layout: 'dashboard',
+            posts,
+        });
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+router.get('/new', auth, (req, res) => {
+    res.render('new-post', {
+        layout: 'dashboard'
+    });
+});
+
+router.get('/edit/:id', auth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render('edit-post', {
+                layout: 'dashboard',
+                post
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
--- controllers/routesHome.js
+++ controllers/routesHome.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({ include: [User] });
         const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('homepage', {posts});
+        res.render('homepage', { posts });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -32,10 +32,26 @@ router.get('/post/:id', auth, async (req, res) => {
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
     res.render('signup');
+});
+
+router.get('/dashboard', auth, async (req,res) => {
+    if (req.session.logged_in) {
+        res.render('dashboard', req.session);
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
